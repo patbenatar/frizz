2431087edec7042e9938c1d3468f8fb0e44f6ca2
Mark Li <mark@philosophie.is>
2016-12-09 14:26:41 -0800

    Add comment

--- lib/frizz/sync.rb
+++ lib/frizz/sync.rb
@@ -9,6 +9,8 @@ module Frizz
       changes = []
 
       # Sync existing files
+      # when comparing remote to local, make sure to strip the .gz extension
+      # from the local files, since none of the remote files will have the gz extension
       remote.files.each do |remote_file|
         local_path = remote_file.key
         local_file = local_index[local_path].sub(/(.gz)$/, '')
